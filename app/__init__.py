a8f02a86002597b144d83affe7111fe3833e7883
David Reid <davidfreid97@gmail.com>
2018-04-30 00:28:27 +0000

    heroku

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = "./app/static/uploads"
 app.config['SECRET_KEY'] = "some$3448eiwjkse3"
+
 #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:secret@localhost/project1"
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://abmszhfntszpqc:1ae49005b227ad8399e0b43ecd8d8b73b516eb8d75485a74e58960ec0ecc8e60@ec2-54-83-23-91.compute-1.amazonaws.com:5432/dej8jsv085l7ip'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
